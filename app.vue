35b9bbb48cf9f29a521f6f374b9f225a80f7760f
lwesley08 <artisticliberties@protonmail.com>
2023-02-05 15:01:08 -0500

    install sass

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+ <Header/>
 </template>
+
+<style lang="scss">
+  body {
+    margin: 0;
+    font-family: Lato;
+    font-weight: 400;
+    background-color: $background-color; // template
+  }
+  body, html {
+    height: 100%;
+  }
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  Hiiiiiiiii
+</template>
